b98a92c0dc43a88de924e16b6d5e52645336255c
Zzzn-1 <363133739@qq.com>
2026-09-02 11:21:59 +0800

    ignore old catkin build artifacts

--- devel_old/share/livox_ros_driver/cmake/livox_ros_driver-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(livox_ros_driver_MESSAGE_FILES "/home/ao/jiguang/LIVO2/ws_livo2/src/ws_livox/src/livox_ros_driver/msg/CustomPoint.msg;/home/ao/jiguang/LIVO2/ws_livo2/src/ws_livox/src/livox_ros_driver/msg/CustomMsg.msg")
-set(livox_ros_driver_SERVICE_FILES "")
